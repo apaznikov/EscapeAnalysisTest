df306230b1fc18ec380354665dfd1759dc5a5278
Alexey Paznikov <apaznikov@gmail.com>
2024-11-28 16:48:12 +0800

    Add IPA recursive call test

--- EscapeAnalysisGlobalTest.c
+++ EscapeAnalysisGlobalTest.c
@@ -6,29 +6,72 @@
 int* GPtr;
 
 //===----------------------------------------------------------------------===//
+// Recursive
+//===----------------------------------------------------------------------===//
+
+/*
+// Simple recursive function
+void rec_func(int *x) {
+	if (*x) {
+		(*x)--;
+		rec_func(x);
+	}
+}
+
+// SCC of 3 mutually recursive functions
+void SCC_foo(int *x);
+void SCC_bar(int *x);
+void SCC_buz(int *x);
+
+void SCC_foo(int *x) {
+	SCC_bar(x);
+}
+
+void SCC_bar(int *x) {
+	SCC_buz(x);
+}
+
+void SCC_buz(int *x) {
+	SCC_foo(x);
+}
+*/
+
+//===----------------------------------------------------------------------===//
 // First test
 //===----------------------------------------------------------------------===//
 
-void callee1_level2(int *x) {
-	GPtr = x;
+void level2_func1(int *x) {
+	GPtr = x;											// Escape through GPtr
 }
 
-void callee1_level1(int *x) {
-	callee1_level2(x);
+void level2_func2(int *x) {
+	*x = 333;
 }
 
-void callee2_level1(int *x) {
+void level1_func1(int *x) {
+	level2_func1(x);
+}
+
+void level1_func2(int *x) {
 	*x = 333;
 	int *p = x;
-	printf("%p\n", &p);
+	printf("%p\n", &p);			// Escape through external func call
+}
+
+void level1_func3(int *x, int *p) {
+	*x = 42;
+	level2_func2(x);
+	level2_func1(p);
 }
 
 void external_func(int *x);
 
-void parent() {
+void parent_func1() {
 	int x;
-	callee1_level1(&x);
-	callee2_level1(&x);
+	level1_func1(&x);
+	level1_func2(&x);
 	int y;
 	external_func(&y);
+	int z, p;
+	level1_func3(&z, &p);
 }
